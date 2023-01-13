181b8ebe79670c50eba4bd8bf0667c5bb051c428
Floris <Floris.robart11@gmail.com>
2023-01-13 12:27:11 +0100

    generateur de compile.list windows fini

--- generateur_compile-list.bat
+++ generateur_compile-list.bat
@@ -1,28 +1,58 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
+    SET "nomFichierChoixDossier=choixDossier.vbs"
 
-SET "source=.\src"
-call :nbDossierInSource "%source%"
+    IF EXIST "%~1" (
+        SET "source=%~1"
+        SET "extensionValide=txt"
+        SET "nomFichierSortie=.\compile.list"
 
+        :: Suppression du fichier de choix de dossier si il existe ::
+        IF EXIST "%nomFichierChoixDossier%" del "%nomFichierChoixDossier%"
+
+        echo.> .\compile.list
+
+        call :listerDossiers
+        call :ListerFichiers
+    ) else (
+        
+
+        echo %nomFichierChoixDossier%
+        echo dossier = inputbox ^("Veuillez entrez le dossier racine à partir du quel la compile.list vas être générer"^, "Séléctionner un dossier"^) > %nomFichierChoixDossier%
+        echo prog = "generateur_compile-list.bat """ ^& dossier ^& """" >> %nomFichierChoixDossier%
+        echo WScript.CreateObject ^("Wscript.shell"^).Run^(prog^), ^0 >> %nomFichierChoixDossier%
+
+        start %nomFichierChoixDossier%
+    )
 ENDLOCAL
 goto :eof
 
 
-::---------------------------------------------------------------::
-:: Détermine le nombre de sous dossier dans les dossiers sources ::
-::---------------------------------------------------------------::
-:nbDossierInSource
-    SET /a "nbDossierInSource=0"
-    FOR /f %%i IN ('dir "%~1" /b /d') DO (
-        :: Afichage de teste
-        echo args = %~1 & echo i = %%i & echo.
+::-------------------------------------------------------------------------------::
+:: Liste les fichiers pour chaque dossiers et sous dossier du dossiers d'origine ::
+::-------------------------------------------------------------------------------::
+:listerDossiers
+    FOR /f %%i IN ('dir "!source!" /b /ad') DO (
+        dir "!source!" /b /a-d 2>nul >nul && ( call :ListerFichiers )
 
+        set "source=%source%\%%i"
 
-        IF %%i == "" (
-            echo pas de sous dossier
-        ) else (
-            :: Appelle récursif
-            call :nbDossierInSource "%source%\%%i"
-        )
+        call :listerDossiers
     )
 goto :eof
+
+
+
+::-----------------------------------------------------------::
+:: Liste les fichiers pour le dossiersen cours nommer source ::
+::-----------------------------------------------------------::
+:ListerFichiers
+    :: liste les fichiers dans le dossier courant ::
+    FOR /f %%i IN ('dir "!source!" /b /a-d') DO (
+        set "extension=%%~xi"
+        IF "%%~xi" == ".%extensionValide%" ( echo !source!\%%i >> %nomFichierSortie% )
+    )
+
+    IF "!extension!" == ".%extensionValide%" ( echo.>> %nomFichierSortie% )
+goto :eof
+
