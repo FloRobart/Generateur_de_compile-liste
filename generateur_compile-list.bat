3f52b3495d9aa38403ef6ba6580605581ab7337c
Floris <Floris.robart11@gmail.com>
2023-01-13 01:52:37 +0100

    ajout des extentions

--- generateur_compile-list.bat
+++ generateur_compile-list.bat
@@ -1,8 +1,28 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-
-
+SET "source=.\src"
+call :nbDossierInSource "%source%"
 
 ENDLOCAL
+goto :eof
+
+
+::---------------------------------------------------------------::
+:: Détermine le nombre de sous dossier dans les dossiers sources ::
+::---------------------------------------------------------------::
+:nbDossierInSource
+    SET /a "nbDossierInSource=0"
+    FOR /f %%i IN ('dir "%~1" /b /d') DO (
+        :: Afichage de teste
+        echo args = %~1 & echo i = %%i & echo.
+
+
+        IF %%i == "" (
+            echo pas de sous dossier
+        ) else (
+            :: Appelle récursif
+            call :nbDossierInSource "%source%\%%i"
+        )
+    )
 goto :eof
