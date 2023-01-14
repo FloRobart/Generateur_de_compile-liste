84a01858b446ef59764d23a9ee71337b0e65b5ca
Floris Robart <florisrobart.pro@gmail.com>
2023-01-14 15:28:19 +0100

    update

--- generateur_compile-list.sh
+++ generateur_compile-list.sh
@@ -30,11 +30,17 @@ ListerFichiers()
         if [ -f $i ]
         then
             extension=${i##*.}
-            if [ $extension == $extensionValide ] then echo $source"/"$i >> $nomFichierSortie fi
+            if [ "$extension" == "$extensionValide" ]
+            then
+                echo $source"/"$i >> $nomFichierSortie
+            fi
         fi
     done
 
-    if [ $extension == $extensionValide ] then echo -e "\n" >> $nomFichierSortie fi
+    if [ "$extension" == "$extensionValide" ]
+    then
+        echo -e "\n" >> $nomFichierSortie
+    fi
 }
 
 
@@ -46,12 +52,15 @@ nomFichierChoixDossier=choixDossier.vbs
 
 if [ $# -gt 0 ]
 then
-    source=$1
-    extensionValide=txt
-    nomFichierSortie=./compile.list
+    source="$1"
+    extensionValide="txt"
+    nomFichierSortie="./compile.list"
 
     # Suppression du fichier de choix de dossier si il existe #
-    if [ -f "$nomFichierChoixDossier" ] then rm -fr "$nomFichierChoixDossier" fi
+    if [ -f "$nomFichierChoixDossier" ]
+    then
+        rm -fr "$nomFichierChoixDossier"
+    fi
 
     echo ""> ./compile.list
 
@@ -61,7 +70,8 @@ else
     
     # Demander à l'utilisateur de choisir un dossier #
     # commande read
-
+    read -p "Veuillez entrez le dossier racine à partir du quel la compile.list vas être générer : " dossier
+    echo dossier = $dossier
     #echo nomFichierChoixDossier
     #echo dossier = inputbox ^("Veuillez entrez le dossier racine à partir du quel la compile.list vas être générer"^, "Séléctionner un dossier"^) > $nomFichierChoixDossier
     #echo prog = "generateur_compile-list.bat """ ^& dossier ^& """" >> $nomFichierChoixDossier
